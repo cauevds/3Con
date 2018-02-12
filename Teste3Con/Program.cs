9c688a52f65df46a0fc6124f88ed16a7e60fa1b2
Caue Vara <caue.vds@gmail.com>
2018-02-12 09:45:55 -0200

    Versão Final

--- Teste3Con/Program.cs
+++ Teste3Con/Program.cs
@@ -11,29 +11,32 @@ namespace Teste3Con
         {
             IController iController = new Controller();
             List<Amigo> listaAmigos = new List<Amigo>();
-
             try
             {
                 listaAmigos = iController.BuscarAmigos();
                 listaAmigos.ForEach(x => x.Localizacao = iController.ObterLatLongPorId(x.Id));
                 var listaAmigosComDistancia = iController.CalcularDistanciaDosAmigos(listaAmigos);
                 var listaAmigosMaisProximos = iController.ObterTresMaisProximos(listaAmigosComDistancia);
-
-                foreach (var amigo in listaAmigosMaisProximos)
-                {
-                    Console.WriteLine("Amigos mais próximos de " + amigo.Nome);
-                    foreach (var amg in amigo.AmigosProximos)
-                    {
-                        Console.WriteLine("-" + amg.Nome);
-                    }
-                    Console.WriteLine("----------------------------------------------");
-                }
-                Console.ReadKey();
+                Apresentar(listaAmigosMaisProximos);
             }
             catch (Exception ex)
             {
                 throw new Exception("Não foi possível buscar os amigos mais próximos - Detalhes: " + ex);
             }           
+        }
+
+        private static void Apresentar(List<Amigo> listaAmigosMaisProximos)
+        {
+            foreach (var amigo in listaAmigosMaisProximos)
+            {
+                Console.WriteLine("Amigos mais próximos de " + amigo.Nome);
+                foreach (var amg in amigo.AmigosProximos)
+                {
+                    Console.WriteLine("-" + amg.Nome);
+                }
+                Console.WriteLine("----------------------------------------------");
+            }
+            Console.ReadKey();
         }
     }
 }
